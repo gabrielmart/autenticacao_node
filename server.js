9365f2d972c8aa386043e7c9c2e1f84849244a23
Gabriel Martins <gabrielmartinsdesouzaa@hotmail.com>
2022-09-15 13:01:19 -0300

    organizando a inicialização dos serviços da API dentro do server.js

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+require('dotenv').config()
 const app  = require('./src/app')
+const db = require('./src/config/dbConnect')
+
+db.on("erro", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+    console.log('conexão com banco feita com sucesso!')
+})
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
-require('dotenv').config()
 const express = require('express')
-const db = require('./config/dbConnect')
 const routes = require('./routes/index')
 const { errorResponder } = require('./middlewares/')
-
-db.on("erro", console.log.bind(console, 'Erro de conexão'))
-db.once("open", () => {
-    console.log('conexão com banco feita com sucesso!')
-})
 
 const app = express()
 routes(app)
