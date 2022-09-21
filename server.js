abca60863a20f61de64f577d5af65963525ac6c8
Gabriel Martins <gabrielmartinsdesouzaa@hotmail.com>
2022-09-21 19:00:49 -0300

    refatoração na conexão do banco de dados

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 require('dotenv').config()
 const app  = require('./src/app')
-const db = require('./src/config/dbConnect')
-
-db.on("erro", console.log.bind(console, 'Erro de conexão'))
-db.once("open", () => {
-    console.log('conexão com banco feita com sucesso!')
-})
+const dbConnect = require('./src/config/dbConnect')
 
 const PORT = process.env.PORT || 3000
 
+dbConnect()
+
 app.listen(PORT, () => {
-    console.log(`App executando na porta ${PORT}`)
+    console.log(`App running on port ${PORT}`)
 })
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,8 +3,22 @@ const mongoose = require('mongoose')
 const DB_USER = process.env.DB_USER
 const DB_PASS = process.env.DB_PASS
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.z0zh7sr.mongodb.net/?retryWrites=true&w=majority`)
+const dbConnect = () => {
+    mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.z0zh7sr.mongodb.net/?retryWrites=true&w=majority`)
 
-const db = mongoose.connection
+    mongoose.connection.on("connected", () => {
+        console.log("Connected to database sucessfully");
+    });
 
-module.exports = db
+    mongoose.connection.on("error", (err) => {
+        console.log("Error while connecting to database :" + err);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+        console.log("Mongodb connection disconnected");
+    });
+}
+
+    
+
+module.exports = dbConnect
